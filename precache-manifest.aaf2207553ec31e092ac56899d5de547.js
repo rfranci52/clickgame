20b05d65f5ccd92d1af0d7a5a75afc5a8b0d9752
Rakim francis <rakimfrancis@Rakims-MBP-2.home>
2019-06-07 17:30:44 -0400

    Updates

---
+++ precache-manifest.aaf2207553ec31e092ac56899d5de547.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36fedabbf696361cbf4508bb9e98e0b",
+    "url": "/clickgame/index.html"
+  },
+  {
+    "revision": "f33f1e014a5bb697b3b4",
+    "url": "/clickgame/static/css/main.d08a38ab.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickgame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "f33f1e014a5bb697b3b4",
+    "url": "/clickgame/static/js/main.5e3c5dbd.chunk.js"
+  },
+  {
+    "revision": "550c79de082ae6f7d9ff",
+    "url": "/clickgame/static/js/runtime~main.5bc58040.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1177445b3cf82993739b917bc945e4af.js"
+  "/clickgame/precache-manifest.aaf2207553ec31e092ac56899d5de547.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clickgame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
